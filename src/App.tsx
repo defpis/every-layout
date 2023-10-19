b68eeecf7dde68bf91ca2822c5442267ce1c5098
温俊 <wenjun.super@bytedance.com>
2023-10-19 23:12:31 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import "./App.scss";
 import { layouts } from "./layouts";
 
+function Cover() {
+  return (
+    <div className="cover">
+      <h1>Every Layout</h1>
+    </div>
+  );
+}
+
 export function App() {
   return (
     <>
-      <h1>Every Layout</h1>
+      <Cover />
 
       {layouts.map((layout) => {
         return <>{layout}</>;
